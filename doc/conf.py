985d939827b6c58c735803e88f11ed1162af6e1b
theo.hollender <theo.hollender@gmail.com>
2024-10-02 14:50:24 +0200

    add-documentation: added versioning sidebar

--- doc/conf.py
+++ doc/conf.py
@@ -6,6 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import importlib.util
+import os
+
+spec   = importlib.util.spec_from_file_location( "generate_gitdata", os.path.join(os.path.dirname( __file__ ), "generate_gitdata.py") )
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
 project = 'polyparser'
 copyright = '2024, polympiads'
 author = 'polympiads'
@@ -19,10 +26,17 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+html_version_root = "https://polympiads.github.io/polyparser/"
+
+html_sidebars = {
+   '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html', 'versions.html'],
+   'using/windows': ['windows-sidebar.html', 'searchbox.html'],
+}
+html_context = module.generate_gitdata(html_version_root)
+print(html_context)
