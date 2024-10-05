2cbf89c170cba35549f37502a227267b0bce8a61
theo.hollender <theo.hollender@gmail.com>
2024-10-05 02:44:49 +0200

    add-lexer: added string lexer rule

---
+++ polyparser/lexer/rules/string.py
@@ -0,0 +1,38 @@
+
+from polyparser.io.reader import FileReader
+from polyparser.lexer.rules.abstract import LexerRule
+from polyparser.lexer.token import Token
+from polyparser.lexer.token.type import TokenType
+from polyparser.utils.optional import Optional
+
+
+class StringLexerRule(LexerRule):
+    def __init__(self, sequence: str, token_type: TokenType):
+        self.sequence   = sequence
+        self.token_type = token_type
+    
+    def poll_sequence (self, reader: "FileReader"):
+        with reader as (atomic, state):
+            if len(state) < len(self.sequence):
+                return False
+            
+            for i in range(len( self.sequence )):
+                if state.poll() != self.sequence[i]:
+                    atomic.rollback()
+                    return False
+            
+            return True
+
+    def try_lexing(self, reader: FileReader) -> Optional[Token]:
+        with reader as (atomic, state):
+            if not self.poll_sequence(reader):
+                atomic.rollback()
+                return None
+
+            while len(state) != 0:
+                if self.poll_sequence(reader):
+                    return Optional(Token(self.token_type, state.as_position()))
+                if state.poll() == '\\':
+                    state.poll()
+
+            atomic.rollback()
---
+++ tests/lexer/rules/test_string.py
@@ -0,0 +1,33 @@
+
+from polyparser.io.reader import FileReader
+from polyparser.lexer import Lexer
+from polyparser.lexer.rules.ignore import IgnoreLexerRule
+from polyparser.lexer.rules.string import StringLexerRule
+
+import string
+
+def test_strings ():
+    all_strings = [
+        "\"abcdef123\"",
+        "\"abc\\\"hi\"",
+        "\"abc\\nhi\""
+    ]
+
+    reader = FileReader( "tests/lexer/rules/file_tests/strings.txt" )
+
+    lexer = Lexer( [
+        StringLexerRule( "\"", "STRING" ),
+        IgnoreLexerRule(string.whitespace)
+    ] )
+
+    words = lexer.try_lexing(reader)
+    assert len(words) == len(all_strings)
+
+    index = 1
+    for word, str in zip(words, all_strings):
+        assert word.token_type == "STRING"
+        assert word.position.column == 1
+        assert word.position.last_column == len(str) + 1
+        assert word.position.height == 1
+        assert word.position.line == index
+        index += 1
